0a6ea7b900c75c143aaf4bf0bcf05531f8f0c353
Egor Khindikaynen <khek@ambertape.ru>
2020-07-09 21:23:09 +0300

    WAVES-104 Showcase.Common: Little fixes.

--- Presentation/Controllers/MainTabPresentationController.cs
+++ Presentation/Controllers/MainTabPresentationController.cs
@@ -10,13 +10,13 @@ namespace Waves.UI.Showcase.Common.Presentation.Controllers
     /// <summary>
     ///     Main Tabs presentation controller.
     /// </summary>
-    public class MainTabPresentationController : PresentationController
+    public abstract class MainTabPresentationController : PresentationController
     {
         /// <summary>
-        /// 
+        /// Creates new instance of <see cref="MainTabPresentationController"/>.
         /// </summary>
-        /// <param name="core"></param>
-        public MainTabPresentationController(Core core)
+        /// <param name="core">Instance of core.</param>
+        protected MainTabPresentationController(Core core)
         {
             Core = core;
         }
@@ -27,34 +27,6 @@ namespace Waves.UI.Showcase.Common.Presentation.Controllers
         public Core Core { get; }
 
         /// <inheritdoc />
-        public override void Initialize()
-        {
-            try
-            {
-                RegisterPresentation(new TextTabPresentation(Core));
-                RegisterPresentation(new ButtonsTabPresentation(Core));
-                RegisterPresentation(new ComboBoxesTabPresentation(Core));
-                RegisterPresentation(new CheckBoxesTabPresentation(Core));
-                RegisterPresentation(new RadioButtonsTabPresentation(Core));
-                RegisterPresentation(new TextBoxesTabPresentation(Core));
-                RegisterPresentation(new ListBoxesTabPresentation(Core));
-                RegisterPresentation(new ProgressBarsTabPresentation(Core));
-                RegisterPresentation(new MenusTabPresentation(Core));
-                RegisterPresentation(new ChartingTabPresentation(Core));
-                RegisterPresentation(new ConfigurationTabPresentation(Core));
-                RegisterPresentation(new CoreTabPresentation(Core));
-                RegisterPresentation(new ThemeTabPresentation(Core));
-                RegisterPresentation(new AboutTabPresentation(Core));
-
-                OnMessageReceived(new Message("Initialization", "Main tab controller initialized.", "Main tab controller", MessageType.Success));
-
-                if (Presentations.Count > 0)
-                    SelectedPresentation = Presentations.First();
-            }
-            catch (Exception e)
-            {
-                OnMessageReceived(new Message("Initialization", "Error initialization main tab controller:\r\n" + e, "Main tab controller", MessageType.Error));
-            }
-        }
+        public abstract override void Initialize();
     }
 }
